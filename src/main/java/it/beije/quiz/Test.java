211ff53fa701b64f3074676e4764ef9cf0341bb4
Padawan09 <Padawan09@DESKTOP-O65P8H4>
2020-02-14 09:57:31 +0100

    Commit pad09

--- src/main/java/it/beije/quiz/Test.java
+++ src/main/java/it/beije/quiz/Test.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Scanner;
 
 import it.beije.quiz.model.Domanda;
+import it.beije.quiz.model.Risposta;
 
 public class Test {
 
@@ -12,7 +13,7 @@ public class Test {
 		List<Domanda> listaDomande = Utils.readFileDomande(PATH);
 		Scanner scan = new Scanner(System.in);
 		for(Domanda dom : listaDomande) {
-			System.out.println(dom.effettuaDomanda());
+			System.out.println(effettuaDomanda(dom));
 			dom.setRispostaUtente(scan.nextLine());
 		}
 		for(Domanda dom : listaDomande) {
@@ -20,5 +21,23 @@ public class Test {
 			if(Utils.controllaRisposta(dom.getRispostaEsatta(), dom.getRispostaUtente())) System.out.println("---------ESATTO---------\n\n");
 			else System.out.println("---------ERRATO---------\n\n");
 		}
+	}
+	
+	public static String effettuaDomanda(Domanda d) {
+		StringBuilder builder = new StringBuilder();
+		builder.append("id : ").append(d.getId()).append('\n');
+		builder.append("book : ").append(d.getBook()).append('\n');
+		builder.append("chapter : ").append(d.getChapter()).append('\n');
+		builder.append("question : ").append(d.getQuestion()).append('\n');
+		builder.append("testo : ").append(d.getTesto()).append('\n');
+		builder.append("answerType : ").append(d.getAnswerType()).append('\n');
+		builder.append("RISPOSTE\n");
+		List<Risposta> risp = d.getRisposte();
+		for (Risposta r : risp) {
+			builder.append("  ").append(r.getValue()).append(" - ").append(r.getText()).append('\n');
+		}
+		builder.append("----------------------------\n");
+		
+		return builder.toString();
 	}
 }
