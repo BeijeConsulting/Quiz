eb998c33bce36da0a9cf3a55bb756a48304d860f
Padawan02 <Padawan02@DESKTOP-I6EQIGK>
2020-02-20 16:25:02 +0100

    co

--- src/main/java/it/beije/quiz/controller/QuizController.java
+++ src/main/java/it/beije/quiz/controller/QuizController.java
@@ -1,5 +1,6 @@
 package it.beije.quiz.controller;
 
+import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
 import java.time.LocalTime;
@@ -9,6 +10,7 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,57 +19,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.annotation.SessionScope;
+import org.xml.sax.SAXException;
 
 import it.beije.quiz.Utils;
 import it.beije.quiz.model.Domanda;
+import it.beije.quiz.model.Libro;
 import it.beije.quiz.model.Risposta;
-
 
 @Controller
 @SessionScope
 public class QuizController {
-	
+
 	private List<Domanda> domande;
 	private int tot;
 	private LocalTime time = null;
+	private List<Libro> libri;
 
-
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-//	@RequestMapping(value = "/home", method = RequestMethod.GET)
-//	public String home(Locale locale, Model model) {
-//		System.out.println("Home Page Requested, locale = " + locale);
-//		Date date = new Date();
-//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-//
-//		String formattedDate = dateFormat.format(date);
-//
-//		model.addAttribute("serverTime", formattedDate);
-//
-//		return "home";
-//	}
-
-//	@RequestMapping(value = "/user", method = RequestMethod.POST)
-//	public String user(@Validated User user, Model model) {
-//		System.out.println("User Page Requested");
-//		model.addAttribute("userName", user.getUserName());
-//		return "user";
-//	}
-	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String init(Model model) {
+	public String init(Model model) throws ParserConfigurationException, SAXException, IOException {
+
+		
+		Utils.popolaLibro(new File("C:\\Users\\Padawan02\\git\\Quiz\\domande\\index.xml"));
+	
 		
 		if (domande == null) {
-			domande = Utils.readFileDomande("C:\\temp\\domande.xml");
+			domande = Utils.readFileDomande("C:\\Users\\Padawan02\\git\\Quiz\\domande\\oca_certification_guide_manning\\domande_cap1.xml");
 			tot = domande.size();
 		}
-		
+
 		model.addAttribute("totDomande", tot);
-		
 		return "index";
 	}
-	
+
 	private void setTimer(Model model) {
 		if (time == null) {
 			time = LocalTime.now();
@@ -76,91 +59,87 @@ public class QuizController {
 		Duration diff = Duration.between(time, now);
 		int tot = domande.size();
 		int secondi = 2 * 60 * tot;
-		long hours = (secondi - diff.getSeconds())/3600;
-		long minutes = (secondi - diff.getSeconds())/60 - hours* 60;
+		long hours = (secondi - diff.getSeconds()) / 3600;
+		long minutes = (secondi - diff.getSeconds()) / 60 - hours * 60;
 		long seconds = (secondi - diff.getSeconds()) - hours * 3600 - minutes * 60;
-		
+
 		model.addAttribute("totDomande", tot);
 		model.addAttribute("ore", hours);
 		model.addAttribute("minuti", minutes);
 		model.addAttribute("secondi", seconds);
 	}
-	
+
 	private String caricaDomanda(Model model, int index) {
 		if (index < tot) {
 			Domanda d = domande.get(index);
 			String risposta = d.getRispostaUtente();
-			//System.out.println("risposta : " + risposta);
+			// System.out.println("risposta : " + risposta);
 			if (risposta == null) {
 				risposta = "";
 			}
 			model.addAttribute("index", index);
-			model.addAttribute("testoDomanda",Utils.formattaTesto(d.getTesto()));
+			model.addAttribute("testoDomanda", Utils.formattaTesto(d.getTesto()));
 			model.addAttribute("rispUtente", risposta);
 			model.addAttribute("answerType", d.getAnswerType());
-			model.addAttribute("risposte",d.getRisposte());
-			
+			model.addAttribute("risposte", d.getRisposte());
+
 			return "domanda";
-		}
-		else {
+		} else {
 			return "riepilogo";
 		}
 	}
-	
+
 	@RequestMapping(value = "/domanda/{index}", method = RequestMethod.GET)
 	public String domanda(Model model, @PathVariable("index") int index) {
-		
+
 		setTimer(model);
-		
+
 		return caricaDomanda(model, index);
 	}
-	
+
 	@RequestMapping(value = "/domanda", method = RequestMethod.POST)
-	public String risposta(Model model, HttpServletRequest request,
-			@RequestParam("index") int index) {
+	public String risposta(Model model, HttpServletRequest request, @RequestParam("index") int index) {
 
 		Enumeration<String> enums = request.getParameterNames();
 		StringBuilder builder = new StringBuilder();
 		while (enums.hasMoreElements()) {
 			String name = enums.nextElement();
-			//System.out.println(name + " : " + request.getParameter(name));
+			// System.out.println(name + " : " + request.getParameter(name));
 			if (name.startsWith("rspt_")) {
 				builder.append(request.getParameter(name));
 			}
 		}
 		domande.get(index).setRispostaUtente(builder.toString());
-		
+
 		setTimer(model);
-		
+
 		return caricaDomanda(model, ++index);
 	}
 
 	@RequestMapping(value = "/risultati", method = RequestMethod.GET)
 	public String risultati(Model model) {
-		//ELABORAZIONE RISPOSTE
+		// ELABORAZIONE RISPOSTE
 		StringBuilder builder = new StringBuilder();
 		for (Domanda d : domande) {
 			boolean corretta = Utils.controllaRisposta(d.getRispostaEsatta(), d.getRispostaUtente());
-			
+
 			builder.append("DOMANDA " + d.getId() + " : la tua risposta : " + d.getRispostaUtente() + "<br><br>");
 			if (corretta) {
 				builder.append("ESATTO!!! :)<br>");
 			} else {
-				builder.append("La risposta esatta era " +  d.getRispostaEsatta() + " :(<br>");
+				builder.append("La risposta esatta era " + d.getRispostaEsatta() + " :(<br>");
 			}
-			
+
 			builder.append("<br><br>");
 		}
-		
+
 		model.addAttribute("body", builder.toString());
-		
+
 		return "risultati";
 	}
-	
-	
-	
+
 	/////// REST
-	
+
 	@RequestMapping(value = "/api/domanda", method = RequestMethod.GET)
 	public void testrest(Model model, HttpServletResponse response) throws IOException {
 		System.out.println("entra??");
@@ -170,7 +149,7 @@ public class QuizController {
 		r.setText("risposta prova");
 		risposte.add(r);
 		Domanda domanda = new Domanda(1, "book", 2, 3, "questa è una prova", "checkbox", risposte, "A", "nessuna");
-		
+
 		response.setContentType("application/json");
 		response.getWriter().append(domanda.toJson());
 	}
---
+++ src/main/java/it/beije/quiz/model/Libro.java
@@ -0,0 +1,39 @@
+package it.beije.quiz.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Libro {
+	
+	private String id_book;
+	private String title;
+	private String dir;
+	private List<Domanda> domande;
+	
+	
+	public List<Domanda> getDomande() {
+		return domande;
+	}
+	public void setDomande(List<Domanda> domande) {
+		this.domande = domande;
+	}
+	public String getId_book() {
+		return id_book;
+	}
+	public void setId_book(String id_book) {
+		this.id_book = id_book;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDir() {
+		return dir;
+	}
+	public void setDir(String dir) {
+		this.dir = dir;
+	}
+
+}
