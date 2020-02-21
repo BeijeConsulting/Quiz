b2c5a3d5902e7494b4687a6bd32155b8f5ce12b1
Padawan05 <Padawan05@DESKTOP-O1ROCKT>
2020-02-21 10:53:05 +0100

    blbl

--- src/main/java/it/beije/quiz/controller/QuizController.java
+++ src/main/java/it/beije/quiz/controller/QuizController.java
@@ -35,25 +35,7 @@ public class QuizController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-//	@RequestMapping(value = "/home", method = RequestMethod.GET)
-//	public String home(Locale locale, Model model) {
-//		System.out.println("Home Page Requested, locale = " + locale);
-//		Date date = new Date();
-//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-//
-//		String formattedDate = dateFormat.format(date);
-//
-//		model.addAttribute("serverTime", formattedDate);
-//
-//		return "home";
-//	}
 
-//	@RequestMapping(value = "/user", method = RequestMethod.POST)
-//	public String user(@Validated User user, Model model) {
-//		System.out.println("User Page Requested");
-//		model.addAttribute("userName", user.getUserName());
-//		return "user";
-//	}
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String init(Model model) {
@@ -67,6 +49,7 @@ public class QuizController {
 		
 		return "index";
 	}
+	
 	
 	private void setTimer(Model model) {
 		if (time == null) {
