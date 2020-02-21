2bf7564d573dad6b72e8390af1fb36422c3785ed
Clark <clarke2897@gmail.com>
2020-02-21 12:36:07 +0100

    Aggiungi domande funzionante, scrittura su xml, mancano metodi dei libri

--- src/main/java/it/beije/quiz/Utils.java
+++ src/main/java/it/beije/quiz/Utils.java
@@ -6,14 +6,20 @@ import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-import it.beije.quiz.model.Domanda;
-import it.beije.quiz.model.Risposta;
+import it.beije.quiz.model.*;
 
 public class Utils {
 	
@@ -109,7 +115,7 @@ public class Utils {
 		        
 	        	Domanda d = new Domanda(id, book, chapter, question, testo, answerType, risposte, rispostaEsatta, spiegazione);
 	        	
-	        	
+	        	arrayDomande.add(d);
 //	        	System.out.println(d);
 	        }	        		
 	        
@@ -142,5 +148,115 @@ public class Utils {
 		
 		return rispostaEsatta.length() == 0;
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/****************BRANCH CLARK*****************/
+	
+	
+	//Scrittura (append) della domanda nel file xml gia esistente
+	public static void aggiungiDomanda(Domanda domanda, File fileXML) {
+		List<Domanda> listDomande= readFileDomande(fileXML.getPath());
+		listDomande.add(domanda);
+		
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder=null;
+        try {
+        	builder = factory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} 
+		
+        Document document = builder.newDocument();
+        Element domande = document.createElement("domande");
+        document.appendChild(domande);
+        
+        for (Domanda d: listDomande) {
+        	
+        	Element dom =document.createElement("domanda");
+        	domande.appendChild(dom);
+        	
+        	dom.setAttribute("id", Integer.toString(d.getId()));
+        	dom.setAttribute("book",d.getBook());
+        	dom.setAttribute("chapter", Integer.toString(d.getChapter()));
+        	dom.setAttribute("question", Integer.toString(d.getQuestion()));
+        	
+        	Element testo=document.createElement("testo");
+        	testo.setTextContent(d.getTesto());
+        	dom.appendChild(testo);
+        	
+        	Element risposte = document.createElement("risposte");        	
+        	risposte.setAttribute("type",d.getAnswerType());
+        	dom.appendChild(risposte);
+        	
+        	List <Risposta> listRisposta = d.getRisposte();
+        	Element risposta=null;
+        	for(Risposta r: listRisposta) {
+        		risposta=document.createElement("risposta");
+        		risposta.setAttribute("type", r.getValue());
+        		risposta.setTextContent(r.getText());
+        		risposte.appendChild(risposta);
+        	}
+        	
+        
+        	
+        	Element risposteEsatte=document.createElement("risposteEsatte");
+        	risposteEsatte.setTextContent(d.getRispostaEsatta());
+        	dom.appendChild(risposteEsatte);
+        	
+        	Element spiegazione= document.createElement("spiegazione");
+        	spiegazione.setTextContent("spiegazione");
+        	dom.appendChild(spiegazione);     	
+        	
+        }
+        
+        
+        
+        
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		Transformer transformer =null;
+		try {
+		 transformer = transformerFactory.newTransformer();
+		} catch (TransformerConfigurationException e) {
+			e.printStackTrace();
+		}
+		
+		DOMSource source = new DOMSource(document);
+		StreamResult result = new StreamResult(fileXML);
+		try {
+			transformer.transform(source, result);
+		} catch (TransformerException e) {
+			e.printStackTrace();
+		}
+		System.out.println("File saved!");
+   
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/******************************************/
 
 }
