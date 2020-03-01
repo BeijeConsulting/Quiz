a8bd748682a82ac0c08e2a4b58a86c182e5336db
0gintama0 <gabry19942009@gmail.com>
2020-03-01 02:50:11 +0100

    ddd

--- src/main/java/it/beije/quiz/controller/QuizController.java
+++ src/main/java/it/beije/quiz/controller/QuizController.java
@@ -31,8 +31,8 @@ public class QuizController {
 	private List<Domanda> domande = new ArrayList<Domanda>();
 	private int tot;
 	private LocalTime time = null;
-	private String pathIndex = "/domande//index.xml";
-	private   List<Libro> listaLibriInXML = Utils.caricaLibriDaIndexXML(pathIndex);
+
+	private   List<Libro> listaLibriInXML = Utils.caricaLibriDaIndexXML("C:\\Users\\Gabriele\\git\\Quiz\\domande\\index.xml");
 	
 	
 	
@@ -77,7 +77,7 @@ public class QuizController {
 		libro.setDir(IDlibro);
 		// libro.setDomanda(null);
 
-		File file = new File(pathIndex);
+		File file = new File("C:\\Users\\Gabriele\\git\\Quiz\\domande\\index.xml");
 		List<Libro> lista = Utils.caricaLibriDaIndexXML(file);
 		lista.add(libro);
 		try {
@@ -92,7 +92,7 @@ public class QuizController {
 		model.addAttribute("listaLibri", listaLibriInXML);
 		
 		StringBuilder dir= new StringBuilder();
-		dir.append("/domande/").append(libro.getDir());
+		dir.append("C:\\Users\\Gabriele\\git\\Quiz\\domande\\").append(libro.getDir());
 		File file1=new File(dir.toString());
 		file1.mkdir();
 		
@@ -270,7 +270,7 @@ public class QuizController {
 		
 		//Clark: al posto di oca_manual ci deve essere directory, per adesso lo metto su oca manual
 		//DISCLAIMER: se vuoi fare il debug cambia il numero del Padawan
-		String path="/domande/"+dir+"/domande_cap"+capitolo+".xml";
+		String path="C:\\Users\\Gabriele\\git\\Quiz\\domande\\"+dir+"\\domande_cap"+capitolo+".xml";
 		
 		File fileXML=new File(path);
 
@@ -289,7 +289,7 @@ public class QuizController {
 	public String aggiungiDomanda(Model model) {
 		
 		 
-		model.addAttribute("listaLibri",Utils.caricaLibriDaIndexXML("domande/index.xml"));
+		model.addAttribute("listaLibri",Utils.caricaLibriDaIndexXML("C:\\Users\\Gabriele\\git\\Quiz\\domande\\index.xml"));
 		model.addAttribute("totDomande", tot);
 		return "aggiungiDomanda";
 		
