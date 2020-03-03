f900cb7deb1a8bb0d6e66c7a63026cae08693b65
Beijeù <Beije@DESKTOP-KB7EVM0>
2020-03-03 11:05:34 +0100

    Test Restbest

--- src/main/java/it/beije/quiz/controller/QuizRestController.java
+++ src/main/java/it/beije/quiz/controller/QuizRestController.java
@@ -1,7 +1,9 @@
 package it.beije.quiz.controller;
 
+
 import java.util.List;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,7 +15,8 @@ import it.beije.quiz.model.Domanda;
 @RestController
 public class QuizRestController {
 
-	@RequestMapping(value = "/restbest", method = RequestMethod.POST)
+	@RequestMapping(value = "/restbest", method = RequestMethod.POST,
+	consumes=MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody Domanda newDomanda(@RequestBody Domanda domanda) {
 		
 		return domanda;
