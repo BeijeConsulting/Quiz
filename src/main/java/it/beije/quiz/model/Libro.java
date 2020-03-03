65b7c28dc96ecfd2c60779bae9026bfe460b039b
Padawan09 <Padawan09@DESKTOP-O65P8H4>
2020-03-03 09:56:27 +0100

    aaa

--- src/main/java/it/beije/quiz/model/Libro.java
+++ src/main/java/it/beije/quiz/model/Libro.java
@@ -13,12 +13,12 @@ public class Libro {
 	private String checked="";
 
 
-	public static final String LIB_PATH="C:\\Users\\Beijeù\\git\\Quiz\\domande\\";
+//	public static final String LIB_PATH="C:\\Users\\Beijeù\\git\\Quiz\\domande\\";
 //	public static final String LIB_PATH=new File("").getAbsolutePath()+"\\domande\\";
 
 
 //	public static final String LIB_PATH="C:\\Users\\Padawan06\\git\\QuizQ\\domande\\";
-//	//public static final String LIB_PATH="C:\\Users\\Padawan09\\git\\Quiz\\domande\\";
+	public static final String LIB_PATH="C:\\Users\\Padawan09\\git\\Quiz\\domande\\";
 //	public static final String LIB_PATH=new File("").getAbsolutePath()+"\\domande\\";
 
 
