c20568cd290db0649b03ded952a3f9d4d389ef98
Brando <b.mancuso37@gmail.com>
2020-10-19 14:39:05 +0200

    update

---
+++ src/main/java/it/beije/quiz/entity/User.java
@@ -0,0 +1,15 @@
+package it.beije.quiz.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "id_user")
+	private Integer id;
+}
