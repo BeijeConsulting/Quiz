ea1bc73fed79d9c3afcec3613ee885bb2cfb4910
danielbassa <daniel.bassanelli@gmail.com>
2020-10-19 16:16:52 +0200

    Upload repositoty into service.

--- src/main/java/it/beije/quiz/service/BookService.java
+++ src/main/java/it/beije/quiz/service/BookService.java
@@ -3,6 +3,8 @@ package it.beije.quiz.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import it.beije.quiz.repository.BookRepository;
+
 @Service
 public class BookService {
 
--- src/main/java/it/beije/quiz/service/UserService.java
+++ src/main/java/it/beije/quiz/service/UserService.java
@@ -1,5 +1,12 @@
 package it.beije.quiz.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import it.beije.quiz.repository.UserRepository;
+
 public class UserService {
 
+	@Autowired
+	private UserRepository resultRepository;
+	
 }
