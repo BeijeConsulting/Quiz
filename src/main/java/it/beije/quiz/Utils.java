6c35742177cda4fafc198b5265b8f43072493e34
Brando <b.mancuso37@gmail.com>
2020-10-22 15:52:56 +0200

    post del fix

--- src/main/java/it/beije/quiz/Utils.java
+++ src/main/java/it/beije/quiz/Utils.java
@@ -4,16 +4,9 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-
-import it.beije.quiz.model.Domanda;
-import it.beije.quiz.model.Risposta;
 
 public class Utils {
 	
@@ -34,66 +27,6 @@ public class Utils {
 		
 		return childElements;
 	}
-
-	/*
-	 * legge il file xml delle domande, richiamando il getchildelements, per salvare in una lista
-	 * tutte le domande con i vari componenti
-	 */
-	public static List<Domanda> readFileDomande(String pathFile) {
-		List<Domanda> arrayDomande = new ArrayList<Domanda>();
-		
-		try {
-			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-	        DocumentBuilder builder = factory.newDocumentBuilder();
-
-	        // Load the input XML document, parse it and return an instance of the
-	        // Document class.
-	        Document document = builder.parse(new File(pathFile));
-	        Element element = document.getDocumentElement();	        
-//	        System.out.println(element.getTagName());
-	        List<Element> domande = Utils.getChildElements(element);
-//	        System.out.println(domande);
-	        	        
-	        List<Element> contenutoDomanda = null;
-	        List<Element> elementiRisposta = null;
-	        Element rispostePossibili = null;
-	        for (Element domanda : domande) {
-	        	contenutoDomanda = Utils.getChildElements(domanda);
-		        int id = Integer.parseInt(domanda.getAttribute("id"));
-		        String book = domanda.getAttribute("book");
-		        int chapter = Integer.parseInt(domanda.getAttribute("chapter"));
-		        int question = Integer.parseInt(domanda.getAttribute("question"));
-		        String testo = contenutoDomanda.get(0).getTextContent();
-		        
-		        //caricare le risposte possibili
-		        rispostePossibili = contenutoDomanda.get(1);
-		        String answerType = rispostePossibili.getAttribute("type");
-		        elementiRisposta = Utils.getChildElements(rispostePossibili);
-		        List<Risposta> risposte = new ArrayList<Risposta>();
-		        for (Element risposta : elementiRisposta) {
-		        	Risposta r = new Risposta();
-		        	r.setValue(risposta.getAttribute("value"));
-		        	r.setText(risposta.getTextContent());
-		        	
-		        	risposte.add(r);
-		        }
-		        
-		        String rispostaEsatta = contenutoDomanda.get(2).getTextContent();
-		        String spiegazione = contenutoDomanda.get(3).getTextContent();
-		        
-	        	Domanda d = new Domanda(id, book, chapter, question, testo, answerType, risposte, rispostaEsatta, spiegazione);
-	        	arrayDomande.add(d);
-	        	
-//	        	System.out.println(d);
-	        }	        		
-	        
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		return arrayDomande;
-	}
-	
 
 	public static String[] listFileNames(File dir) {
 		String[] fileNames = dir.list();
--- src/main/java/it/beije/quiz/controller/QuestionController.java
+++ src/main/java/it/beije/quiz/controller/QuestionController.java
@@ -1,29 +1,20 @@
 package it.beije.quiz.controller;
 
-import java.time.Duration;
-import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import it.beije.quiz.Utils;
 import it.beije.quiz.entity.Answer;
 import it.beije.quiz.entity.Question;
 import it.beije.quiz.entity.Test;
-import it.beije.quiz.entity.User;
-import it.beije.quiz.model.Domanda;
-import it.beije.quiz.model.Storico;
 import it.beije.quiz.service.QuestionService;
 
 @Controller
