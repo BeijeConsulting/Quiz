95c9af65eec6a4a39984e92ef24a7f3d5d99e487
Giuseppe Madonia <madonia.giuseppe8@gmail.com>
2020-10-22 22:03:43 +0200

    js per le domande quasi completate

--- WebContent/WEB-INF/views/domandaAjax.jsp
+++ WebContent/WEB-INF/views/domandaAjax.jsp
@@ -21,7 +21,11 @@
 <div>Bottoni</div> -->
 
 <div id="question_body">Corpo Domanda</div>
-<div id="answers_body">Corpo Risposte</div>
+<div id="answers_body">Corpo Domanda</div>
+<div id="page_buttons">
+	<button id="button_prev" type="button" onclick="onClickPrev()">Prec</button>
+	<button id="button_next" type="button" onclick="onClickNext()">Succ</button>
+</div>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
--- WebContent/resources/js/domandaAjax.js
+++ WebContent/resources/js/domandaAjax.js
@@ -1,7 +1,8 @@
-let questions = [{id: 524, testo:"Ciao", answerType: "checkbox"}, {id: 525, testo:"Arrivederci", answerType: "radio"}]
+let questions = [{id: 524, text:"Ciao", answerType: "checkbox"}, {id: 525, text:"Arrivederci", answerType: "radio"}];
 let indexQuestions = 0;
 let answered = [];
 
+// Get the answers to a given question id
 async function getAnswers(idQuestion) {
 	let path = getContextPath();
 	let answers = await fetch(path + "/rest/risposte/domanda/" + idQuestion)
@@ -10,6 +11,7 @@ async function getAnswers(idQuestion) {
 	return answers;
 }
 
+// Get the questions to a given quiz id
 async function getQuestions(idQuiz) {
 	let path = getContextPath();
 	let questions = await fetch(path + "/rest/domande/quiz/" + idQuiz)
@@ -18,22 +20,27 @@ async function getQuestions(idQuiz) {
 	return questions;
 }
 
+// Shows the question
 function setQuestionBody(index) {
 	let questionBody = document.getElementById("question_body");
 	let question = questions[index];
 	
-	questionBody.innerHTML = question.testo;
+	questionBody.innerHTML = question.text;
 }
 
-async function setAnswersBody(idQuestion) {
+// Populate the section of the answers
+async function setAnswersBody(index) {
 	let answersBody = document.getElementById("answers_body");
-	let answers = await getAnswers(idQuestion);
-	buildAnswersBody(answers);
-}
-
-function buildAnswersBody(answers) {
+	let answers = await getAnswers(questions[index].id);
+	
+	// Clean
+	while(answersBody.firstChild) {
+		answersBody.removeChild(answersBody.lastChild);
+	}
+	
+	// Populate
 	for(let i = 0; i < answers.length; i++) {
-		answersBody.appendChild(buildAnswer(questions[0], answers[i]));
+		answersBody.appendChild(buildAnswer(questions[index], answers[i]));
 	}
 }
 
@@ -41,7 +48,11 @@ function buildAnswer(question, answer) {
 	let answerBlock = document.createElement("div");
 	let input = document.createElement("input");
 	input.type = question.answerType;
-	input.name = "answer_" + answer.letter;
+	if (input.type == "checkbox") {
+		input.name = "answer_" + answer.letter;
+	} else if (input.type == "radio"){
+		input.name = "answer_radio";
+	}
 	input.id = input.name;
 	input.value = answer.id;
 	input.onchange = function () { onInputChange(this) };
@@ -54,8 +65,66 @@ function buildAnswer(question, answer) {
 	return answerBlock;
 }
 
-function addAnswer() {
-	
+function updateAnswered(input) {
+	if (input.type == "checkbox") {
+		if (input.checked) {
+			if (!answered.includes(input.value)) {
+				answered.push(input.value);
+			}
+		} else {
+			let index = answered.indexOf(input.value);
+			let newAnswered = [];
+			for(let i = 0; i < answered.length; i++) {
+				if (i != index) {
+					newAnswered.push(answered[i]);
+				}
+			}
+			answered = newAnswered;
+		}
+	} else if (input.type == "radio") {
+		answered[0] = input.value;
+	}
+}
+
+function nextQuestion() {
+	if (indexQuestions < questions.length - 1) {
+		indexQuestions++;
+		setQuestionBody(indexQuestions);
+		setAnswersBody(indexQuestions);
+		setButtonsVisibility();
+	}
+}
+
+function prevQuestion() {
+	if (indexQuestions > 0) {
+		indexQuestions--;
+		setQuestionBody(indexQuestions);
+		setAnswersBody(indexQuestions);
+		setButtonsVisibility();
+	}
+}
+
+function setButtonsVisibility() {
+	nextButtonVisibility();
+	prevButtonVisibility();
+}
+
+function nextButtonVisibility() {
+	let button = document.getElementById("button_next");
+	if (indexQuestions < questions.length - 1) {
+		button.style.visibility = "visible";
+	} else {
+		button.style.visibility = "hidden";
+	}
+}
+
+function prevButtonVisibility() {
+	let button = document.getElementById("button_prev");
+	if (indexQuestions > 0) {
+		button.style.visibility = "visible";
+	} else {
+		button.style.visibility = "hidden";
+	}
 }
 
 function getContextPath() {
@@ -63,10 +132,19 @@ function getContextPath() {
 }
 
 function onPageLoad() {
-	setQuestionBody(0);
-	setAnswersBody(524);
+	setQuestionBody(indexQuestions);
+	setAnswersBody(indexQuestions);
+	setButtonsVisibility();
 }
 
 function onInputChange(input) {
-	
+	updateAnswered(input);
+}
+
+function onClickNext() {
+	nextQuestion();
+}
+
+function onClickPrev() {
+	prevQuestion();
 }
