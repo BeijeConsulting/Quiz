6d1b8d2e1f0455958d0cd69f6a7a49f2106e44cd
Padawan04 <Padawan04@DESKTOP-CJ91L3V.station>
2020-10-22 18:02:40 +0200

    fixare answerservice

---
+++ WebContent/WEB-INF/views/quiz_view.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>FUNZIA</title>
+</head>
+<body>
+<a href="./question/0">iniziaTEST</a>
+
+</body>
+</html>
--- src/main/java/it/beije/quiz/service/AnswerService.java
+++ src/main/java/it/beije/quiz/service/AnswerService.java
@@ -2,6 +2,10 @@ package it.beije.quiz.service;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,14 +18,17 @@ public class AnswerService {
 
 	@Autowired
 	private AnswerRepository answerRepo;
-
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	@Transactional
 	public List<Answer> createAnswers(List<Question> questions, int test_id) {
 		for(int i = 0; i < questions.size(); i++) {
 			Answer a = new Answer();
 			a.setIndex(i+1);
 			a.setQuestion(questions.get(i).getId());
 			a.setTest(test_id);
-			answerRepo.saveAndFlush(a);
+			log.debug(""+a.getQuestion());
+			answerRepo.save(a);
 		}
 		return answerRepo.findByTest(test_id);
 		
