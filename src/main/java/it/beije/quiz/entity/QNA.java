451a30ad13422ebed5b44510a99423e7cd793ad7
h4rk <ale.sala.97@gmail.com>
2020-10-22 17:21:48 +0200

    transient bean with display pourposes

---
+++ src/main/java/it/beije/quiz/entity/QNA.java
@@ -0,0 +1,56 @@
+package it.beije.quiz.entity;
+
+public class QNA {
+
+	private Integer questionId;
+	private Integer answerId;
+	private String test;
+	private String answerType;
+	private String options;
+	private String userChoice;
+	
+	
+	public Integer getQuestionId() {
+		return questionId;
+	}
+	public void setQuestionId(Integer questionId) {
+		this.questionId = questionId;
+	}
+	public Integer getAnswerId() {
+		return answerId;
+	}
+	public void setAnswerId(Integer answerId) {
+		this.answerId = answerId;
+	}
+	public String getTest() {
+		return test;
+	}
+	public void setTest(String test) {
+		this.test = test;
+	}
+	public String getAnswerType() {
+		return answerType;
+	}
+	public void setAnswerType(String answerType) {
+		this.answerType = answerType;
+	}
+	public String getOptions() {
+		return options;
+	}
+	public void setOptions(String options) {
+		this.options = options;
+	}
+	public String getUserChoice() {
+		return userChoice;
+	}
+	public void setUserChoice(String userChoice) {
+		this.userChoice = userChoice;
+	}
+	@Override
+	public String toString() {
+		return "QNA [questionId=" + questionId + ", answerId=" + answerId + ", test=" + test + ", answerType="
+				+ answerType + ", options=" + options + ", userChoice=" + userChoice + "]";
+	}
+	
+	
+}
