bc795dacc647e401dd98a68f0ed8b0821107c698
Padawan06 <Padawan06@DESKTOP-9RDOHCL.station>
2020-10-23 14:04:21 +0200

    user homepg

--- src/main/java/it/beije/quiz/controller/UserHomeRestController.java
+++ src/main/java/it/beije/quiz/controller/UserHomeRestController.java
@@ -1,8 +1,6 @@
 package it.beije.quiz.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -10,18 +8,10 @@ import javax.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import it.beije.quiz.entity.QNA;
-import it.beije.quiz.entity.Question;
 import it.beije.quiz.entity.Result;
-import it.beije.quiz.repository.AnswerRepository;
-import it.beije.quiz.service.AnswerService;
-import it.beije.quiz.service.QuestionService;
 import it.beije.quiz.service.ResultService;
 
 
@@ -37,11 +27,11 @@ public class UserHomeRestController {
 	@GetMapping("/history")
 	public List<Result> getUserHistory(HttpServletRequest request) {
 		HttpSession session = request.getSession(false);
+		log.info("user id: "+session.getAttribute("userid"));
 		if(session==null) return null;
 		else {
-			System.out.println("user id: "+session.getAttribute("userId"));
-			//session.setAttribute("userId", new Integer(1));
-			List<Result> history = resultService.findByUserId((Integer)session.getAttribute("userId"));
+
+			List<Result> history = resultService.findByUserId((Integer)session.getAttribute("userid"));
 			return history;
 		}
 	}
