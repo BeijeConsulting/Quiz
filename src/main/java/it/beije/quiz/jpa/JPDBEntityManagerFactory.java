e5c280eec79b7e2acaff02503693b70751b2b2e7
Luca Gregori <lucagregori96@gmail.com>
2020-10-26 09:17:23 +0100

    Done 1st stable build

---
+++ src/main/java/it/beije/quiz/jpa/JPDBEntityManagerFactory.java
@@ -0,0 +1,16 @@
+package it.beije.quiz.jpa;
+//import java.util.HashMap;
+//import java.util.Map;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+public class JPDBEntityManagerFactory {
+	private JPDBEntityManagerFactory() {}
+	private static EntityManagerFactory factory;
+	public static EntityManager createEntityManager() {
+		if (factory == null) {
+			factory = Persistence.createEntityManagerFactory("Quiz");
+		}
+		return factory.createEntityManager();
+	}
+}
